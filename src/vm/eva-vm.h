a38102830b113edce468561fdec7b8bd0d2cdf27
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-02-14 10:22:09 +0200

    *

---
+++ src/vm/eva-vm.h
@@ -0,0 +1,30 @@
+
+#ifndef EVA_VM_h
+#define EVA_VM_h
+#include <string>
+#include <vector>
+// Eva Virtual Machine
+
+class EvaVirtualMachine {
+  public:
+    EvaVirtualMachine(){}
+
+
+
+    // 
+    void exe(const std::string &program){
+
+
+
+      ip
+    }
+
+// ip
+u_int8_t* ip;
+    // byte code
+    std::vector<u_int8_t> code;
+};
+
+
+
+#endif
