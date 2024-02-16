4b28bc6ccb050e2514e3e7680e5cafe84464ec5e
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-02-16 20:38:11 +0200

    *

---
+++ src/beytecode/operational-code.h
@@ -0,0 +1,12 @@
+#ifndef OperationalCode_h
+#define OperationalCode_h
+
+// stop the program
+#define OPERATIONAL_HALT 0x00
+
+// push a const in into the stack
+#define OPERATIONAL_CONST 0x01
+#endif
+
+//
+//
