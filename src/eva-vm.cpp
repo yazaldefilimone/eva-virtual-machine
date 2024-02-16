5e20c34d76f717a513e78966436f5bfc7999ef5e
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-02-16 04:43:28 +0200

    *

--- src/eva-vm.cpp
+++ src/eva-vm.cpp
@@ -1,8 +1,11 @@
 #include "./vm/eva-vm.h"
 
+#include <iostream>
+
 int main(int argc, char const *argv[]) {
   EvaVirtualMachine vm;
   vm.exec(R"(45)");
 
+  std::cout << "All done!" << std::endl;
   return 0;
 }
